60144679a3c622cf74b55ec34a1a868e974d3052
Abdul6498 <abdulrehman6498@outlook.com>
2020-12-20 10:53:19 +0100

    Check the values of pixel greater than 15

--- test/test.cpp
+++ test/test.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <array>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/utility.hpp>
 
@@ -11,31 +12,53 @@ using namespace cv;
 void load_images(const String& dirname, vector< Mat >& img_lst);
 int main()
 {
-    int pixel_value1 = 0, pixel_value2 = 0;
-
+    int pixel_value1 = 0, pixel_value2=0, pixel_value[100][100], count=0;
+    
     cout << "Hello World!\n";
     //Load All images at once
     string path = "Test_images/*.bmp";
     vector<Mat> images_fn;
 
-
     load_images(path, images_fn);
-    resize(images_fn[1], images_fn[1], Size(), 0.25, 0.25);
+    cout << "Size of Mat: " << images_fn[1].channels() << endl;
+    resize(images_fn[1], images_fn[1], Size(1280,1024));
+    //Mat img = imread("Test_images/2.bmp");
+    cvtColor(images_fn[1], images_fn[1], cv::COLOR_BGR2GRAY);
     imshow("Image window", images_fn[1]);
 
     //Mat img = imread("Test_images/1.bmp", IMREAD_GRAYSCALE);
-    cout << images_fn[3].cols;
-  /*  for (int x = 0; x <= img.rows; x++)
-    {
-        for (int y = 0; y <= img.cols; y++)
-        {
-            pixel_value2 = img.at<uchar>(x, y);
+    //cout << "Cols: "<<images_fn[1].cols << "\n";
+    //cout << "Rows: " << images_fn[1].rows << "\n";
 
-            cout << "pixel_value2: " << pixel_value2 << endl;
+    //pixel_value = new int[images_fn[1].rows][images_fn[1].cols];
+
+    for (int x = 0; x < images_fn[1].rows; x++)
+    {
+        //cout << "Row" << x << "\n";
+        for (int y = 0; y < images_fn[1].cols; y++)
+        { 
+            //pixel_value[x][y] = images_fn[1].at<uchar>(x, y);
+
+            if (images_fn[1].at<uchar>(x, y) > 15)
+            {
+                pixel_value2 = images_fn[1].at<uchar>(x, y);
+                //cout << "pixel_value: " << pixel_value2 << endl;
+                //cout << "x:" << x << "-- y:" << y << endl;
+                count++;
+            }
+
+            //cout << "pixel_value: " << pixel_value[x][y] << endl;
         }
-    }*/
-    pixel_value2 = images_fn[1].at<uchar>(200, 200);
-    cout << "pixel_value2: " << pixel_value2 << endl;
+    }
+
+    cout << "Count: " << count << endl;
+
+    for (int x = 0; x <= count; x++)
+    {
+        //cout << "pixel_value2: " << pixel_value[9][x] << endl;
+    }
+    pixel_value1 = images_fn[1].at<uchar>(513-1, 642-1);
+    cout << "pixel_value1: " << pixel_value1 << endl;
     waitKey(0);
     return 0;
 }
