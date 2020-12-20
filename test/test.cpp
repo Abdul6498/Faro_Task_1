8c402cc63244811c4ee9186c2d902b346273ada0
Abdul6498 <abdulrehman6498@outlook.com>
2020-12-21 00:39:01 +0100

    Create new function for processing images

--- test/test.cpp
+++ test/test.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <array>
+#include <fstream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/utility.hpp>
 
@@ -10,55 +11,29 @@ using namespace std;
 using namespace cv;
 
 void load_images(const String& dirname, vector< Mat >& img_lst);
+void process_images(vector< Mat >& img_in);
+
+ofstream MyFile("Coordinates.txt");
+
 int main()
 {
-    int pixel_value1 = 0, pixel_value2=0, pixel_value[100][100], count=0;
-    
-    cout << "Hello World!\n";
-    //Load All images at once
+    int pixel_value1[3] = {0,0,0}, pixel_value2 = 0, pixel_value3 = 0, pixel_value[100][100], count = 0, dist = 1, count_nb = 0;
     string path = "Test_images/*.bmp";
     vector<Mat> images_fn;
 
+    cout << "Hello World!\n";
+    //Load All images at once
+  
+
     load_images(path, images_fn);
-    cout << "Size of Mat: " << images_fn[1].channels() << endl;
-    resize(images_fn[1], images_fn[1], Size(1280,1024));
-    //Mat img = imread("Test_images/2.bmp");
-    cvtColor(images_fn[1], images_fn[1], cv::COLOR_BGR2GRAY);
-    imshow("Image window", images_fn[1]);
+    process_images(images_fn);
+    //resize(images_fn[1], images_fn[1], Size(1280,1024));
 
-    //Mat img = imread("Test_images/1.bmp", IMREAD_GRAYSCALE);
-    //cout << "Cols: "<<images_fn[1].cols << "\n";
-    //cout << "Rows: " << images_fn[1].rows << "\n";
+   // cout << "No of Channels in grey: " << images_fn[1].channels() << endl;
+   // imshow("Image window", images_fn[1]);
 
-    //pixel_value = new int[images_fn[1].rows][images_fn[1].cols];
-
-    for (int x = 0; x < images_fn[1].rows; x++)
-    {
-        //cout << "Row" << x << "\n";
-        for (int y = 0; y < images_fn[1].cols; y++)
-        { 
-            //pixel_value[x][y] = images_fn[1].at<uchar>(x, y);
-
-            if (images_fn[1].at<uchar>(x, y) > 15)
-            {
-                pixel_value2 = images_fn[1].at<uchar>(x, y);
-                //cout << "pixel_value: " << pixel_value2 << endl;
-                //cout << "x:" << x << "-- y:" << y << endl;
-                count++;
-            }
-
-            //cout << "pixel_value: " << pixel_value[x][y] << endl;
-        }
-    }
-
-    cout << "Count: " << count << endl;
-
-    for (int x = 0; x <= count; x++)
-    {
-        //cout << "pixel_value2: " << pixel_value[9][x] << endl;
-    }
-    pixel_value1 = images_fn[1].at<uchar>(513-1, 642-1);
-    cout << "pixel_value1: " << pixel_value1 << endl;
+    MyFile.close();
+    //rows,cloumns
     waitKey(0);
     return 0;
 }
@@ -74,3 +49,28 @@ void load_images(const String& dirname, vector< Mat >& img_lst)
 
 }
 
+void process_images(vector< Mat >& img_in)
+{
+    int pixel_value1[3] = { 0,0,0 }, dist = 1;
+    for (int i = 0; i < img_in.size(); i++)
+    {
+        MyFile << "Image No: " << i << endl;
+        cvtColor(img_in[i], img_in[i], cv::COLOR_RGB2GRAY);
+        for (int x = 0; x < img_in[i].rows; x++)
+        {
+            for (int y = 0; y < img_in[i].cols; y = y + dist)
+            {
+                if (img_in[i].at<uchar>(x, y) > 15)
+                {
+                    pixel_value1[0] = img_in[i].at<uchar>(x, y);
+                    pixel_value1[1] = img_in[i].at<uchar>(x, y + 1);
+                    pixel_value1[2] = img_in[i].at<uchar>(x, y + 2);
+                    if (pixel_value1[1] >= pixel_value1[0] && pixel_value1[1] >= pixel_value1[2])
+                    {
+                        MyFile << "Row: " << x + 1 << " --   Col: " << y + 2 << " --    Pixel Value: " << pixel_value1[1] << endl;
+                    }
+                }
+            }
+        }
+    }
+}
