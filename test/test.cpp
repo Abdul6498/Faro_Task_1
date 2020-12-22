4612becb003e46185ea1da0a4e229a1e8cd2de4a
Abdul6498 <abdulrehman6498@outlook.com>
2020-12-22 01:40:05 +0100

    Version 3: All the requirement has been fulfilled

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,13 @@
-// test.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+Description : Loads all image files within a folder into memory and processes one after the other. 
+Convert the attached Loaded files to gray scale. Get the positions of the lines in 
+the image. The processing deliver all image pixel coordinates where the pixel brightness 
+of the image after it has been averaged over +/-5 pixels horizontally is maximal within a neighbourhood of
+horizontally +/- 10 pixels.
+Author Name : Abdul Rehman
+contact: abdulrehman6498@outlook.com
+Version : 3.0
+*/
 
 #include <iostream>
 #include <array>
@@ -10,69 +18,35 @@
 using namespace std;
 using namespace cv;
 
-void load_images(const String& dirname, vector< Mat >& img_lst);
-void process_images(vector< Mat >& img_in);
-void average_fn(vector< Mat >& in_img);
+unsigned volatile kernel_size = 11;     //Kernel Size for averging filter
+unsigned volatile kernel_size_max = 10; //Kernel Size for maximal filter
+unsigned volatile kernel_size_U = 21;   //Upper Limit of Kernel Size for maximal filter (-/+ 10 pixels = 10 Neighbors from left + 1 Centre pixel + 10 Neighbors from Right)
+unsigned volatile dist = 1;             //Distance for maximum filter
 
-ofstream MyFile("Coordinates.txt");
+void load_images(const String& dirname, vector< Mat >& img_lst);    //Function to load images
+void process_images(vector< Mat >& img_in);                         //Function to Process images. For finding the maximum pixel value
+void average_fn(vector< Mat >& in_img);                             //Function to find the average
+
+ofstream MyFile("Coordinates.txt");                                 //File name to record the coordinates and pixel values
 
 int main()
 {
-    unsigned int pixel_values[5];
-    int  avg_val = 0, count = 0;
+    string path_test = "Test_images/*.bmp";                         //Path for input raw images
+    string path_prc = "Prepared_images/*.bmp";                      //Path for averaged images 
 
-    string path = "Test_images/*.bmp";
-    vector<Mat> images_fn;
-    //Vec3b  color;
+    vector<Mat> images_avg;
+    vector<Mat> images_prc;
+
     cout << "Starting From Scratch, Hello Dr Rolf From Abdul Rehman!\n";
 
-    load_images(path, images_fn);   //Load All bmp images at once  
-   // process_images(images_fn);  //Process images and write coordinates to the file
-   // average_fn(images_fn);
-    cvtColor(images_fn[1], images_fn[1], cv::COLOR_RGB2GRAY);
-    //cout << "No of channels: " << images_fn[1].channels() << endl;
+    load_images(path_test, images_avg);   //Load All bmp images at once
+    average_fn(images_avg);               //Take average
 
-    for (int y = 0; y < images_fn[1].rows; y++)
-    {
-        for (int x = 0; x < images_fn[1].cols; x++)
-        {
-            //if (images_fn[1].at<uchar>(y, x) > 1)
-            //{
-            pixel_values[0] = images_fn[1].at<uchar>(y, x);
-            count++;
-            if (count == 0)
-            {
-                pixel_values[0] = images_fn[1].at<uchar>(y, x);
-            }
-            else if (count == 1)
-            {
-                pixel_values[1] = images_fn[1].at<uchar>(y, x);
-            }
-            else if (count == 2)
-            {
-                pixel_values[2] = images_fn[1].at<uchar>(y, x);
-                avg_val = (pixel_values[0] + pixel_values[1] + pixel_values[2]) * 3;
-                images_fn[1].at<uchar>(y, x - 1) = avg_val;
-                count = 0;
-            }
-            //pixel_values[1] = images_fn[1].at<uchar>(y, x);
-            //pixel_values[2] = images_fn[1].at<uchar>(y, x + 1);
-            //pixel_values[2] = images_fn[1].at<uchar>(y, x + 3);
-            //pixel_values[2] = images_fn[1].at<uchar>(y, x + 4);
-            //avg_val = (pixel_values[0] + pixel_values[1] + pixel_values[2]) / 3;
-            //images_fn[1].at<uchar>(y, x) = avg_val;
-        //}
-        }
-    }
-    //images_fn[1].at<uchar>(265, 680) = 255;
-    //pixel_vlaue3 = images_fn[1].at<uchar>(265, 680);
-    //cout << "Channel 1: " << pixel_vlaue3 << endl;
-    //cout << "Channel 2: " << color.val[1] << endl;
-    //cout << "Channel 3: " << color.val[2] << endl;
-    imwrite("Test_images/new_image.jpg", images_fn[1]);
-    //resize(images_fn[1], images_fn[1], Size(1280,1024));
+    load_images(path_prc, images_prc);   //Load All bmp images at once
+    process_images(images_prc);  //Process images and write coordinates to the file
+    
 
-    MyFile.close();
+    MyFile.close();             //Close the file after recording
 
     waitKey(0);
     return 0;
@@ -91,49 +65,102 @@ void load_images(const String& dirname, vector< Mat >& img_lst)
 
 void process_images(vector< Mat >& img_in)
 {
-    int pixel_value1[3] = { 0,0,0 }, dist = 1;
-    for (int i = 0; i < img_in.size(); i++)
+    int pxl_prv = 0, pxl_crnt = 0, pxl_prv_U = 0, pxl_crnt_U = 0, pxl_center = 0;
+
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~Processing Images~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    cout << "Please wait software is Processing test images." << endl;
+
+    for (int i = 0; i < img_in.size(); i++)                                                     //Most outer loop to process images one by one
     {
-        MyFile << "Image No: " << i + 1 << endl;
-        cvtColor(img_in[i], img_in[i], cv::COLOR_RGB2GRAY);
-        for (int x = 0; x < img_in[i].rows; x++)
+        pxl_prv = 0, pxl_crnt = 0, pxl_prv_U = 0, pxl_crnt_U = 0, pxl_center = 0;
+
+        cout << "Processing of image no " << i + 1 << " has started..................." << endl;
+
+        MyFile << "Image No: " << i + 1 << endl;                                               //Data input to file
+
+        cvtColor(img_in[i], img_in[i], cv::COLOR_RGB2GRAY);                                    //Change the image format 3 channels to Gray scale
+
+        for (int x = 0; x < img_in[i].rows; x++)                                               //First inner loop(Nested Loop) for Rows in the image(Iteration size: 0 to No. of Rows)
         {
-            for (int y = 0; y < img_in[i].cols; y = y + dist)
+            for (int y = 0; y < img_in[i].cols- kernel_size_U; y = y + dist)                   //Second inner loop(Nested Loop) for Coloumns in the image(Iteration size: 0 to No. of cols-upper limit of kernel)
             {
-                if (img_in[i].at<uchar>(x, y) > 15)
+                for (int p = y; p < kernel_size_max + y; p++)                                  //Find the maximum number from the first 10 pixels
                 {
-                    pixel_value1[0] = img_in[i].at<uchar>(x, y);
-                    pixel_value1[1] = img_in[i].at<uchar>(x, y + 1);
-                    pixel_value1[2] = img_in[i].at<uchar>(x, y + 2);
-                    if (pixel_value1[1] >= pixel_value1[0] && pixel_value1[1] >= pixel_value1[2])
+                    pxl_crnt = img_in[i].at<uchar>(x, p);
+                    if (pxl_crnt >= pxl_prv)
                     {
-                        MyFile << "Row: " << x + 1 << " --   Col: " << y + 2 << " --    Pixel Value: " << pixel_value1[1] << endl;
+                        pxl_prv = pxl_crnt;
                     }
                 }
+
+                for (int p = kernel_size_max+y+1; p < kernel_size_U + y; p++)                  //Find the maximum number from the last 10 pixels
+                {
+                    pxl_crnt_U = img_in[i].at<uchar>(x, p);
+                    if (pxl_crnt_U >= pxl_prv_U)
+                    {
+                        pxl_prv_U = pxl_crnt_U;
+                    }
+                }
+
+                if (pxl_prv_U > 15 && pxl_prv > 15)                                             //Find local maxima
+                {
+                    pxl_center = img_in[i].at<uchar>(x, abs(kernel_size_max + y));
+                     if (pxl_center >= pxl_prv_U && pxl_center >= pxl_prv)
+                    {
+                        MyFile << "Row: " << x + 1 << " --   Col: " << y + kernel_size << " --    Pixel Value: " << pxl_center << endl;
+                    }
+                }               
             }
         }
+        cout << "Processing of image no " << i + 1 << " has finished.!!!!!!!!!!!!!!!!!!!!!!" << endl;
     }
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~Processing completed~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
 }
 
 void average_fn(vector< Mat >& img_in)
 {
-    int pixel_value1[3] = { 0,0,0 }, dist = 1;
-    for (int i = 0; i < img_in.size(); i++)
-    {
-        MyFile << "Image No: " << i + 1 << endl;
-        cvtColor(img_in[i], img_in[i], cv::COLOR_RGB2GRAY);
-        for (int x = 0; x < img_in[i].rows; x++)
-        {
-            for (int y = 0; y < img_in[i].cols; y = y + dist)
-            {
-                if (img_in[i].at<uchar>(x, y) > 15)
-                {
-                    pixel_value1[0] = img_in[i].at<uchar>(x, y);
-                    pixel_value1[1] = img_in[i].at<uchar>(x, y + 1);
-                    pixel_value1[2] = img_in[i].at<uchar>(x, y + 2);
+    unsigned int pixel_values;
+    unsigned int  avg_val = 0, count = 0, sum_pxl = 0, k = 0;
 
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~Avergae Started~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    cout << "Please wait software is taking average of the test images." << endl;
+
+    for (int t = 0; t < img_in.size(); t++)                                                 //Most outer loop to process images one by one
+    {
+        cout << "Calculating the average of image no "<< t+1 << " has started........" << endl;
+
+        cvtColor(img_in[t], img_in[t], cv::COLOR_RGB2GRAY);                                 //Change the image format 3 channels to Gray scale
+
+        for (int y = 0; y < img_in[t].rows; y++)                                            //First inner loop(Nested Loop) for Rows in the image(Iteration size: 0 to No. of Rows)
+        {
+            for (int i = 0; i < img_in[t].cols - kernel_size; i++)                          //Second inner loop(Nested Loop) for Coloumns in the image(Iteration size: 0 to No. of cols - kernel size)
+            {
+                for (int x = i; x < i + kernel_size; x++)                                   //Apply moving average
+                {
+
+                    pixel_values = img_in[t].at<uchar>(y, x);
+                    sum_pxl = pixel_values + sum_pxl;
+
+                    if (count == (kernel_size - 1))
+                    {
+                        k = abs(x - (kernel_size/2));
+                        avg_val = sum_pxl / kernel_size;
+                        img_in[t].at<uchar>(y, k) = avg_val;                                //Change the brightness of pixel according to the average
+                        sum_pxl = 0;
+                        avg_val = 0;
+                        count = 0;
+                    }
+                    else
+                    {
+                        count++;
+                    }
                 }
             }
+
         }
+        cout << "Avergae Completed for image no " << t + 1 << " has finished!!!!!!!!!!!" << endl;
+        imwrite("Prepared_images/image_" + to_string(t+1) + ".bmp", img_in[t]);
     }
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~Avergae completed~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+   // process_images(img_in);   //Process the images without saving(Optional)//Don't uncomment without the permission of Author. 
 }
